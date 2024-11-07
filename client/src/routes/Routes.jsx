c4eb8c43568f624e59a7ec67f0040bd439c5be64
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-08 00:30:46 +0600

    added

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,98 +1,112 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../layouts/Main'
-import Home from '../pages/Home/Home'
-import ErrorPage from '../pages/ErrorPage'
-import Login from '../pages/Login/Login'
-import SignUp from '../pages/SignUp/SignUp'
-import RoomDetails from '../pages/RoomDetails/RoomDetails'
-import PrivateRoute from './PrivateRoute'
-import DashboardLayout from '../layouts/DashboardLayout'
-import Statistics from '../pages/Dashboard/Common/Statistics'
-import Mylistings from '../pages/Dashboard/Hosts/Mylistings'
-import RoomAdd from '../pages/Dashboard/Hosts/RoomAdd'
-import Profile from '../pages/Dashboard/Common/Profile'
-import ManageUsers from '../pages/Dashboard/Admin/ManageUsers'
-import AdminRoute from './AdminRoute'
-import HostRoute from './HostRoute'
-import MyBookings from '../pages/Dashboard/Guests/MyBookings'
-import ManageBookings from '../pages/Dashboard/Hosts/ManageBookings'
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import RoomDetails from "../pages/RoomDetails/RoomDetails";
+import PrivateRoute from "./PrivateRoute";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Statistics from "../pages/Dashboard/Common/Statistics";
+import Mylistings from "../pages/Dashboard/Hosts/Mylistings";
+import RoomAdd from "../pages/Dashboard/Hosts/RoomAdd";
+import Profile from "../pages/Dashboard/Common/Profile";
+import ManageUsers from "../pages/Dashboard/Admin/ManageUsers";
+import AdminRoute from "./AdminRoute";
+import HostRoute from "./HostRoute";
+import MyBookings from "../pages/Dashboard/Guests/MyBookings";
+import ManageBookings from "../pages/Dashboard/Hosts/ManageBookings";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/room/:id',
-        element: <PrivateRoute>
-          <RoomDetails />
-        </PrivateRoute>,
+        path: "/room/:id",
+        element: (
+          <PrivateRoute>
+            <RoomDetails />
+          </PrivateRoute>
+        ),
       },
     ],
   },
-  { path: '/login', element: <Login /> },
-  { path: '/signup', element: <SignUp /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> },
   {
-    path: '/dashboard',
-    element: <PrivateRoute>
-      <DashboardLayout></DashboardLayout>
-    </PrivateRoute>,
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
     children: [
       {
         index: true,
-        element: <PrivateRoute>
-          <Statistics/>
-        </PrivateRoute>,
+        element: (
+          <PrivateRoute>
+            <Statistics />
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'add-room',
-         element: <PrivateRoute>
-          <HostRoute>
-          <RoomAdd />
-          </HostRoute>
-         </PrivateRoute>
+        path: "add-room",
+        element: (
+          <PrivateRoute>
+            <HostRoute>
+              <RoomAdd />
+            </HostRoute>
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'my-listings',
-         element: <PrivateRoute>
-          <HostRoute>
-          <Mylistings/>
-          </HostRoute>
-         </PrivateRoute>
+        path: "my-listings",
+        element: (
+          <PrivateRoute>
+            <HostRoute>
+              <Mylistings />
+            </HostRoute>
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'manage-bookings',
-         element: <PrivateRoute>
-          <HostRoute>
-          <ManageBookings/>
-          </HostRoute>
-         </PrivateRoute>
+        path: "manage-bookings",
+        element: (
+          <PrivateRoute>
+            <HostRoute>
+              <ManageBookings />
+            </HostRoute>
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'my-bookings',
-         element: <PrivateRoute>
-          
-          <MyBookings/>
-          
-         </PrivateRoute>
+        path: "my-bookings",
+        element: (
+          <PrivateRoute>
+            <MyBookings />
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'manage-users',
-         element: <PrivateRoute>
-          <AdminRoute>
-          <ManageUsers/>
-          </AdminRoute>
-         </PrivateRoute>
+        path: "manage-users",
+        element: (
+          <PrivateRoute>
+            <AdminRoute>
+              <ManageUsers />
+            </AdminRoute>
+          </PrivateRoute>
+        ),
       },
       {
-         path: 'profile',
-         element: <Profile/>
+        path: "profile",
+        element: <Profile />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
