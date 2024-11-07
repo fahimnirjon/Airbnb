ce5bb6b5d555ecca9ee2f56120c818ed048d8e9e
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-08 00:35:43 +0600

    added

--- client/src/components/Dashboard/TableRow/RoomDataRow.jsx
+++ client/src/components/Dashboard/TableRow/RoomDataRow.jsx
@@ -3,16 +3,14 @@ import { format } from "date-fns";
 import DeleteModal from "../../Modal/DeleteModal";
 import { useState } from "react";
 
-const RoomDataRow = ({ room,handleDelete }) => {
-
+const RoomDataRow = ({ room, handleDelete }) => {
   // delete modal
-  let [isOpen, setIsOpen] = useState(false)
-  const closeModal = () =>{
-    setIsOpen(false)
-  }
+  let [isOpen, setIsOpen] = useState(false);
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   // update modal
-   
 
   return (
     <tr>
@@ -49,14 +47,22 @@ const RoomDataRow = ({ room,handleDelete }) => {
         </p>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <button onClick={()=> setIsOpen(true)} className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
             aria-hidden="true"
             className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
           ></span>
           <span className="relative">Delete</span>
         </button>
-        <DeleteModal isOpen={isOpen} closeModal={closeModal} handleDelete={handleDelete} id={room?._id} />
+        <DeleteModal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          handleDelete={handleDelete}
+          id={room?._id}
+        />
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <span className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
@@ -79,7 +85,6 @@ RoomDataRow.propTypes = {
 
 export default RoomDataRow;
 
-
 RoomDataRow.propTypes = {
-  handleDelete: PropTypes.func
-}
+  handleDelete: PropTypes.func,
+};
